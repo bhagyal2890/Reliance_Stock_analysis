c4eb229fb2ac17fe4cdccd772a33d1ce22a37547
Bhagyalakshmi.G <132869350+bhagyal2890@users.noreply.github.com>
2023-07-06 22:58:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -181,7 +181,7 @@ if bt:
     print(closedf.shape)
     
     # Split data into training and testing sets
-    train_size= int(len(closedf)*0.86)
+    training_size= int(len(closedf)*0.86)
     test_size=len(closedf)-training_size
     train_data,test_data=closedf[0:training_size,:],closedf[training_size:len(closedf),:1]
     print("train_data: ", train_data.shape)
