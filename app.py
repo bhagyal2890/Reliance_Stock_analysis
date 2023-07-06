3dffe389f845b85d1532e0ef20bb0253f1d8a72d
Bhagyalakshmi.G <132869350+bhagyal2890@users.noreply.github.com>
2023-07-06 19:51:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,3 +65,37 @@ if bt:
     plt.xlabel('Date')
     plt.ylabel('High Price')
     st.pyplot(fig=plt)
+    
+    st.header("Graphs- Box Plot")
+    plt.figure(figsize=(20,10))
+
+    # Creating box-plots
+    plt.figure(figsize=(20,10))
+    #Plot 1
+    plt.subplot(2,2,1)
+    plt.boxplot(reliance['Open'])
+    plt.xlabel('Date')
+    plt.ylabel('Open Price')
+    plt.title('Open')
+    st.pyplot(fig=plt)
+    #Plot 2
+    plt.subplot(2,2,2)
+    plt.boxplot(reliance['Close'])
+    plt.xlabel('Date')
+    plt.ylabel('Cloes Price')
+    plt.title('Close')
+    st.pyplot(fig=plt)
+    #Plot 3
+    plt.subplot(2,2,3)
+    plt.boxplot(reliance['High'])    
+    plt.xlabel('Date')
+    plt.ylabel('High Price')
+    plt.title('High')
+    st.pyplot(fig=plt)
+    #Plot 4
+    plt.subplot(2,2,4)
+    plt.boxplot(reliance['Low'])
+    plt.xlabel('Date')
+    plt.ylabel('Low Price')
+    plt.title('Low')
+    st.pyplot(fig=plt)
