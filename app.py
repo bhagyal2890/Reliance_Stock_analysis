abfbafe6c07add3d672a47e2320b3f42d1982b0c
Bhagyalakshmi.G <132869350+bhagyal2890@users.noreply.github.com>
2023-07-06 19:21:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,131 +1,64 @@
-import os
-
-import numpy as np
 import pandas as pd
-import tensorflow as tf
-from sklearn.preprocessing import MinMaxScaler
-from pathlib import Path
-from datetime import datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
+import yfinance as yf
+import datetime
+import streamlit as st
+import model_building as m
 
 
-def predict_stock_price():
-    # Read data
-    df = pd.read_csv("./RELIANCE_5yrs.csv")
+with st.sidebar:
+    st.markdown("# Reliance_Stock_Analysis")
+    user_input = st.multiselect('Please select the stock',['RELIANCE.NS'],['RELIANCE.NS'])
 
-    # Get close price data
-    data = df.filter(['Close']).values
+    # user_input = st.text_input('Enter Stock Name', "ADANIENT.NS")
+    st.markdown("### Choose Date for your anaylsis")
+    START = st.date_input("From",datetime.date(2015, 1, 1))
+    END = st.date_input("To",datetime.date(2023, 2, 28))
+    bt = st.button('Submit') 
 
-    # Scale data between 0 and 1
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    scaled_data = scaler.fit_transform(data)
+#adding a button
+if bt:
 
-    # Split data into training and testing sets
-    training_data_len = int(len(data) * 0.8)
-    train_data = scaled_data[0:training_data_len, :]
-    x_train = []
-    y_train = []
+# Importing dataset------------------------------------------------------
+    df = yf.download('RELIANCE.NS', start=START, end=END)
+    plotdf, future_predicted_values =m.create_model(df)
+    df.reset_index(inplace = True)
+    st.title('Reliance Stock Market Prediction')
+    st.header("Data We collected from the source")
+    st.write(df)
 
-    for i in range(10, len(train_data)):
-        x_train.append(train_data[i - 10:i, 0])
-        y_train.append(train_data[i, 0])
+    reliance_1=df.drop(["Adj Close"],axis=1).reset_index(drop=True)
+    reliance_2=reliance_1.dropna().reset_index(drop=True)
 
-    x_train, y_train = np.array(x_train), np.array(y_train)
-
-    # Reshape data for LSTM model
-    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-
-    # Define LSTM model
-    model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.LSTM(100, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-    model.add(tf.keras.layers.LSTM(100, return_sequences=False))
-    model.add(tf.keras.layers.Dense(25))
-    model.add(tf.keras.layers.Dense(1))
-
-    # Compile and fit the model
-    model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
-    model.fit(x_train, y_train, batch_size=32, epochs=100)
-
-    # Create testing data set
-    test_data = scaled_data[training_data_len - 10:, :]
-    x_test = []
-    y_test = data[training_data_len:, :]
-
-    for i in range(10, len(test_data)):
-        x_test.append(test_data[i - 10:i, 0])
-
-    x_test = np.array(x_test)
-    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-
-    # Make predictions
-    predictions = model.predict(x_test)
-    predictions = scaler.inverse_transform(predictions)
-
-    # Predict the future trend
-    future_data = df.filter(['Close'])
-    last_60_days = future_data[-10:].values
-    last_60_days_scaled = scaler.transform(last_60_days)
-    X_test = []
-    X_test.append(last_60_days_scaled)
-    X_test = np.array(X_test)
-    X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-    future_prediction = model.predict(X_test)
-    future_prediction = scaler.inverse_transform(future_prediction)
-    print('Predicted price for the next day:', future_prediction[0][0])
-
-    # make predictions for the next 10 days
-    future_data = df.filter(['Close'])
-    last_X_days = future_data[-800:] # here you can change as I tried with 800 last time
-    last_X_days_scaled = scaler.transform(last_X_days)
-    X_test = []
-    X_test.append(last_X_days_scaled)
-    X_test = np.array(X_test)
-    X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-    predicted_prices = []
-    for i in range(10):
-        predicted_price = model.predict(X_test)
-        predicted_prices.append(predicted_price[0][0])
-        X_test = np.append(X_test, [predicted_price], axis=1)
-
-    # invert the scaling
-    predicted_prices = np.array(predicted_prices).reshape(-1, 1)
-    predicted_prices = scaler.inverse_transform(predicted_prices)
-
-    # print the predicted prices
-    print(predicted_prices)
-
-    # # Append predicted prices to Excel file, for GitHub Actions only
-    # today = datetime.now().strftime("%Y-%m-%d")
-    # filename = 'predicted_prices.xlsx'
-    #
-    # if not Path(filename).is_file():
-    #     df_predicted = pd.DataFrame(predicted_prices.reshape(1, -1), columns=['Day ' + str(i) for i in range(1, 11)])
-    #     df_predicted.index = [today]
-    #     df_predicted.to_excel(filename)
-    #     print("made and inserted")
-    # else:
-    #     df_predicted = pd.read_excel(filename, index_col=0)
-    #     df_predicted['Day ' + str(df_predicted.shape[1] + 1)] = predicted_prices.reshape(1, -1)
-    #     df_predicted.to_excel(filename)
-    #     print("read and inserted")
-
-    # Append predicted prices to Excel file , use this block only for local OS
-
-    # Append predicted prices to Excel file
-    today = datetime.now().strftime("%Y-%m-%d")
-    filename = 'predicted_prices.xlsx'
-
-    df_predicted = pd.DataFrame(predicted_prices.reshape(1, -1), columns=['Day ' + str(i) for i in range(1, 11)])
-    df_predicted.index = [today]
-
-    if not Path(filename).is_file():
-        df_predicted.to_excel(filename)
-        print("created and inserted")
-    else:
-        df_existing = pd.read_excel(filename, index_col=0)
-        df_combined = pd.concat([df_existing, df_predicted], axis=0)
-        df_combined.to_excel(filename)
-        print("appended and inserted")
+    reliance=reliance_2.copy()
+    reliance['Date']=pd.to_datetime(reliance['Date'],format='%Y-%m-%d')
+    reliance=reliance.set_index('Date')
+    st.title('EDA')
+    st.write(reliance)
 
 
-# calling function
-predict_stock_price()
+# ---------------------------Graphs--------------------------------------
+
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.title('Visualizations')
+
+    st.header("Graphs")
+    plt.figure(figsize=(20,10))
+    #Plot 1
+    plt.subplot(2,2,1)
+    plt.plot(reliance['Open'],color='green')
+    plt.xlabel('Date')
+    plt.ylabel('Open Price')
+    plt.title('Open')
+    #Plot 2
+    plt.subplot(2,2,2)
+    plt.plot(reliance['Close'],color='red')
+    plt.xlabel('Date')
+    plt.ylabel('Close Price')
+    plt.title('Close')
+    #Plot 3
+    plt.subplot(2,2,3)
+    plt.plot(reliance['High'],color='green')
+    plt.xlabel('Date')
+    plt.ylabel('High Price')
